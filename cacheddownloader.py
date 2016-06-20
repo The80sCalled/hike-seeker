c2e3ef2343761b037f89c9870d63a9874ec31e00
The80sCalled <fischer.mt@gmail.com>
2016-06-20 21:37:24 +0800

    Six feet trip info parsing

--- cacheddownloader.py
+++ cacheddownloader.py
@@ -1,19 +1,29 @@
 import requests
+import osutils
+import os
+import codecs
+import logging
 
 class CachedDownloader:
 
-    def __init__(self, cache_folder):
+    def __init__(self, cache_folder, use_cache):
         self.cache_folder = cache_folder
         self.download_count = 0
+        self.use_cache = use_cache
         osutils.ensure_dir(self.cache_folder)
 
-    def download_with_cache(self, url):
+    def download(self, url):
         """
         Downloads example sentences for the given word from Bing's dictionary.
         Returns the content of the html page.
         :param word: hanzi of word to search for
         :return:
         """
+        if not self.use_cache:
+            response = requests.get(url)
+            response_as_text = response.content.decode(encoding=response.encoding)
+            return response_as_text
+
         cache_file = os.path.join(self.cache_folder, osutils.make_valid_filename(url) + ".txt")
 
         if not os.path.exists(cache_file):
@@ -32,8 +42,3 @@ class CachedDownloader:
         else:
             with codecs.open(cache_file, 'r', 'utf-8') as file:
                 return file.read()
-
-    def download_without_cache(self, url):
-        response = requests.get(url)
-        response_as_text = response.content.decode(encoding=response.encoding)
-        return response_as_text
--- sixfeet.py
+++ sixfeet.py
@@ -1,16 +1,22 @@
 # Downloads tracks from foooooot.com.  "Six Feet" is the Chinese name of the site
 
 import cacheddownloader
+import os
+import osutils
+import shutil
+import unittest
 
 class SixFeetDownloader:
 
-    _FOOT_BASE_URL = "http://foooooot.com
+    _FOOT_BASE_URL = "http://foooooot.com"
 
     # The '2' means Beijing
-    _TRACK_LISTING_URL = "/search/trip/all/2/all/occurtime/descent/?page={0}&keyword="
+    _TRIP_LISTING_URL = "/search/trip/all/2/all/occurtime/descent/?page={0}&keyword="
 
-    def __init__(self, downloader):
-        self.downloader = downloader
+    def __init__(self, trip_downloader, track_downloader):
+        self.trip_downloader = trip_downloader
+        self.track_downloader = track_downloader
+
     #
     # def _parse_sentences_bing(self, dom):
     #     dom_sentences = dom.xpath('//*[@class="se_li"]')
@@ -42,46 +48,76 @@ class SixFeetDownloader:
     #         sentences.append({ "english": english, "chinese": chinese})
     #
     #     return sentences
+    def _parse_track_info_dom(self, dom):
+        import re
+        import datetime
+
+        dom_trips = dom.xpath('//div[@class="listSection"]//li[not(@class)]//dl')
+        infos = []
+
+        for s in list(dom_trips):
+            trip_url = s.xpath('.//p[@class="trip-title"]/a[1]/@href')[0]
+            matches = re.search("^/trip/(\d*)/$", trip_url)
+            trip_id = matches.group(1)
+
+            trip_title = s.xpath('.//p[@class="trip-title"]/a[1]/text()')[0].strip()
+
+            start_time_text = s.xpath('./dd[2]/text()')[0].strip()
+            start_time_text = re.search("([-:\d\s]{8,20})", start_time_text).group(1).strip()
+            trip_start_time = datetime.datetime.strptime(start_time_text, "%Y-%m-%d %H:%M")
+
+            infos.append(TripInfo(trip_id, trip_title, trip_start_time))
+
+        return infos
 
 
-    def get_beijing_track_info(self, word):
+    def get_beijing_trip_info(self, num_pages):
         """Downloads basic track summary info"""
         from lxml import html
 
-        # TODO: urllib.parse.urlencode seems to order its parameters however it feels like it,
-        # TODO: which messes with the caching logic. So, yeah
-        #query_string = urllib.parse.urlencode({ "q": word })
-        url = SixFeetDownloader._FOOT_BASE_URL + SixFeetDownloader._TRACK_LISTING_URL.Format("1")
+        trip_info = []
 
-        dom = html.fromstring(self.downloader.download_with_cache(url))
+        for i in range(1, num_pages + 1):
+            url = SixFeetDownloader._FOOT_BASE_URL + SixFeetDownloader._TRIP_LISTING_URL.format("1")
+            dom = html.fromstring(self.trip_downloader.download(url))
+            trip_info.extend(self._parse_track_info_dom(dom))
 
-        return self._parse_sentences_bing(dom)
+        return trip_info
 
 
-#     def get_sentences_iciba(self, word):
-#         """Downloads example sentences for a given word from Bing"""
-#         from lxml import html
-#
-#         url = SentenceDownloader._ICIBA_BASE_URL + "/" + word + "-1.html"
-#         dom = html.fromstring(self.downloader.download_with_cache(url))
-#
-#         return self._parse_sentences_iciba(dom)
-#
-#
-#
+class TripInfo:
+    def __init__(self, id, title, hike_date):
+        self.id = id
+        self.title = title
+        self.hike_date = hike_date
 
 
 class UnitTests(unittest.TestCase):
-    _TEMP_SENTENCE_CACHE_FOLDER = os.path.expandvars("$Temp\\sentences_py_unittest_cache")
+    _TEMP_SENTENCE_CACHE_FOLDER = os.path.expandvars("$Temp\\sixfeet_py_unittest_cache")
 
     def setUp(self):
         osutils.clear_dir(UnitTests._TEMP_SENTENCE_CACHE_FOLDER)
+        shutil.copytree("unittest\\test-data", UnitTests._TEMP_SENTENCE_CACHE_FOLDER, True)
 
     def tearDown(self):
         pass
 
     def test_track_list(self):
-        downloader =
+        import datetime
+
+        me = SixFeetDownloader(cacheddownloader.CachedDownloader(UnitTests._TEMP_SENTENCE_CACHE_FOLDER, True),
+                               cacheddownloader.CachedDownloader(UnitTests._TEMP_SENTENCE_CACHE_FOLDER, True))
+
+        trips = me.get_beijing_trip_info(1)
+        self.assertEqual(10, len(trips))
+
+        self.assertEqual("672456", trips[0].id)
+        self.assertEqual("22公里测试", trips[0].title)
+
+        self.assertEqual("748475", trips[1].id)
+        self.assertEqual("大觉寺 萝卜地 妙峰山 阳台山 凤凰岭 白虎涧", trips[1].title)
+
+        self.assertEqual(datetime.datetime(2106, 2, 7, 14, 28), trips[2].hike_date)
 
 # class UnitTests(unittest.TestCase):
 #     _TEMP_SENTENCE_CACHE_FOLDER = os.path.expandvars("$Temp\\sentences_py_unittest_cache")
