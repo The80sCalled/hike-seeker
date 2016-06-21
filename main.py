414c95728aaf49bafe5a7d2f0cb7a108d8607781
The80sCalled <fischer.mt@gmail.com>
2016-06-21 11:45:52 +0800

    Visualize track start and stop

--- main.py
+++ main.py
@@ -41,7 +41,10 @@ def _download_tons_of_trips(config, page_range, use_delay):
             trips = jsonpickle.decode(file.read())
         logging.info("Loaded {0} trips from cache".format(len(trips)))
 
-    for t in trips[3424:]:
+    # DEBUG: only load some trips
+    trips = trips[0:1000]
+
+    for t in trips: # [4000:]
         # foooooot.com returns HTTP 599 if you query it too quickly.  Introduce a random delay.
         if use_delay:
             time.sleep(random.randrange(2, 6))
@@ -59,13 +62,13 @@ if __name__ == "__main__":
 
     INCLUDE_PAGE_RANGE = range(1, 2451)
 
-    trips = _download_tons_of_trips(config, INCLUDE_PAGE_RANGE, use_delay=True)
+    trips = _download_tons_of_trips(config, INCLUDE_PAGE_RANGE, use_delay=False)
     original_trips_len = len(trips)
 
-    # skip_ids = {"748475"}
-    # activity_types = {"登山", "徒步"}
-    # trips = [t for t in trips if t.id not in skip_ids and t.type in activity_types]
-    # logging.info("Loaded {0} trips, {1} meet filter criteria".format(original_trips_len, len(trips)))
-    #
-    # os.makedirs(config['kml_output_folder'], exist_ok=True)
-    # tripvis.visualize_trips(trips, os.path.join(config['kml_output_folder'], "BeijingHikes.kml"))
+    skip_ids = {"748475"}
+    activity_types = {"登山", "徒步"}
+    trips = [t for t in trips if t.id not in skip_ids and t.type in activity_types]
+    logging.info("Loaded {0} trips, {1} meet filter criteria".format(original_trips_len, len(trips)))
+
+    os.makedirs(config['kml_output_folder'], exist_ok=True)
+    tripvis.visualize_trips(trips, os.path.join(config['kml_output_folder'], "BeijingHikes.kml"))
--- tripvis.py
+++ tripvis.py
@@ -3,22 +3,93 @@ from lxml import etree
 import codecs
 import logging
 
+def _start_end_marker_coords_from_point(lat, long):
+    import math
+
+    KM_PER_DEGREE = 10000 / 90
+    CIRCLE_RADIUS_IN_METERS = 100
+    RADIUS_Y = CIRCLE_RADIUS_IN_METERS / 1000 / KM_PER_DEGREE
+    RADIUS_X = RADIUS_Y * math.cos(42 / (180 / math.pi))
+
+    POLY_SEGS = 9
+    coords = ""
+    for r in range(POLY_SEGS + 1):
+        rads = r / POLY_SEGS * 2 * math.pi
+        coords += "{0},{1} ".format(long + RADIUS_X * math.cos(rads), lat + RADIUS_Y * math.sin(rads))
+
+    return coords
 
 def _placemark_from_trip_and_track(trip_info):
     coords = ""
     for p in trip_info.track:
         coords += "{0},{1} ".format(p.longitude, p.latitude)
 
-    return KML.Placemark(
-        KML.name(trip_info.id),
-        KML.styleUrl("#hikeTrack"),
+    start_coords = ""
+    end_coords = ""
 
-        KML.LineString(
-            KML.altitudeMode("clampToGround"),
+    if len(trip_info.track) > 0:
+        start_coords = _start_end_marker_coords_from_point(trip_info.track[0].latitude, trip_info.track[0].longitude)
+        end_coords = _start_end_marker_coords_from_point(trip_info.track[-1].latitude, trip_info.track[-1].longitude)
 
-            KML.coordinates(coords)
+    return [
+        KML.Placemark(
+            KML.name(trip_info.id),
+            KML.styleUrl("#hikeTrack"),
+
+            KML.LineString(
+                KML.altitudeMode("clampToGround"),
+
+                KML.coordinates(coords)
+            )
+        ),
+
+        KML.Placemark(
+            KML.name(trip_info.id + "-start"),
+            KML.styleUrl("#hikeStart"),
+
+            KML.LineString(
+                KML.altitudeMode("clampToGround"),
+
+                KML.coordinates(start_coords)
+            )
+        ),
+
+        KML.Placemark(
+            KML.name(trip_info.id + "-end"),
+            KML.styleUrl("#hikeEnd"),
+
+            KML.LineString(
+                KML.altitudeMode("clampToGround"),
+
+                KML.coordinates(end_coords)
+            )
         )
-    )
+    ]
+
+        # KML.Polygon(
+        #     KML.outerBoundaryIs(
+        #         KML.LinearRing(
+        #             KML.coordinates(start_coords)
+        #         )
+        #     )
+        # ),
+        #
+        # KML.Polygon(
+        #     KML.outerBoundaryIs(
+        #         KML.LinearRing(
+        #             KML.coordinates(end_coords)
+        #         )
+        #     )
+        # )
+
+# < Polygon > < outerBoundaryIs > < LinearRing >
+# < coordinates >
+# 135.2, 35.4, 0.
+# 135.4, 35.6, 0.
+# 135.2, 35.6, 0.
+# 135.2, 35.4, 0.
+# < / coordinates >
+# < / LinearRing > < / outerBoundaryIs > < / Polygon >
 
 
 def visualize_trips(trips, kml_file):
@@ -32,11 +103,25 @@ def visualize_trips(trips, kml_file):
                 KML.width("3")
             ),
             id="hikeTrack"
+        ),
+        KML.Style(
+            KML.LineStyle(
+                KML.color("7f00ff00"),
+                KML.width("10")
+            ),
+            id="hikeStart"
+        ),
+        KML.Style(
+            KML.LineStyle(
+                KML.color("7f0000ff"),
+                KML.width("10")
+            ),
+            id="hikeEnd"
         )
     )
 
     for trip in trips:
-        kml_doc.append(
+        kml_doc.extend(
             _placemark_from_trip_and_track(trip)
         )
 
